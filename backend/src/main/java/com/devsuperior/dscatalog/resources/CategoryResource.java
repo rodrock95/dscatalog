b8bcd8aa87b5f494c845f802816dce98cbee3af6
rodrock95 <r.barboza9502@gmail.com>
2022-03-19 18:07:04 -0300

    Category by id

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,14 +1,17 @@
 package com.devsuperior.dscatalog.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
@@ -18,10 +21,18 @@ public class CategoryResource {
 	@Autowired
 	private CategoryService service;
 	
+	//buscar todas as categorias
 	@GetMapping
 	public ResponseEntity<List<CategoryDTO>> findAll(){
 		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	//buscar uma categoria por um id
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +21,15 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll(){
 		List<Category> list = repository.findAll();
-				
 		List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		return listDto;
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		//optional é uma abordagem para evitar trabalhar com valores nulos
+		Category entity = obj.get(); //obtém objeto dentro do optional
+		return new CategoryDTO(entity);
 	}
 }
